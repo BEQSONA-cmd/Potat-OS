0b3b236ddc6e78b63a02607dd8691cb05a8c89df
Beqa <tvildiani2001@gmail.com>
2025-08-08 14:01:51 +0200

    updates

--- front/src/components/contexts/FileContext.tsx
+++ front/src/components/contexts/FileContext.tsx
@@ -49,7 +49,7 @@ interface getRepoParams {
 
 async function getRepo({ repoName }: getRepoParams) {
     try {
-        const response = await axios.get(`${HOST}/api/get`, {
+        const response = await axios.get(`${HOST}/api/test`, {
             params: {
                 repoName: repoName,
             },
