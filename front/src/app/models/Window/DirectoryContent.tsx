7734eacdd1b1bda45199eb1aa58de12a078a832b
Beqa <tvildiani2001@gmail.com>
2025-08-10 11:23:51 +0200

    updates

--- front/src/app/models/Window/DirectoryContent.tsx
+++ front/src/app/models/Window/DirectoryContent.tsx
@@ -1,42 +1,27 @@
 import { I_File, useFiles } from "@/components/contexts/FileContext";
 import { FaFile, FaFolder } from "react-icons/fa";
 import NameInput from "@/components/Files/NameInput";
+import { getFileIcon } from "@/components/Files/utils";
 
-function DirectoryFiles({ files }: { files: I_File[] }) {
+function FileInDirectory({ file }: { file: I_File }) {
     const { editFileId, openFile } = useFiles();
 
     function onDoubleClick(e: React.MouseEvent, file: I_File) {
         openFile(file.id);
     }
-
+    const Icon = getFileIcon(file);
     return (
-        <div className="h-full w-full p-4">
-            <div className="grid grid-cols-auto-fill gap-2">
-                {files.map((file) => (
-                    <div key={file.id} className="flex flex-col items-center justify-center w-full">
-                        <div
-                            onDoubleClick={(e) => onDoubleClick(e, file)}
-                            className="text-xs font-medium text-white group flex flex-col items-center w-full cursor-pointer p-2 hover:bg-gray-700 rounded transition-colors"
-                        >
-                            {file.type === "directory" ? (
-                                <FaFolder
-                                    size={40}
-                                    className="text-yellow-500 group-hover:text-yellow-400 transition-colors"
-                                />
-                            ) : (
-                                <FaFile
-                                    size={40}
-                                    className="text-blue-500 group-hover:text-blue-400 transition-colors"
-                                />
-                            )}
-                            {editFileId === file.id ? (
-                                <NameInput file={file} />
-                            ) : (
-                                <span className="mt-1 truncate w-full text-center">{file.name}</span>
-                            )}
-                        </div>
-                    </div>
-                ))}
+        <div key={file.id} className="flex flex-col items-center justify-center w-full">
+            <div
+                onDoubleClick={(e) => onDoubleClick(e, file)}
+                className="text-xs font-medium text-white group flex flex-col items-center w-full cursor-pointer p-2 hover:bg-gray-700 rounded transition-colors"
+            >
+            <Icon size={40} className="text-white transition-colors" />
+                {editFileId === file.id ? (
+                    <NameInput file={file} />
+                ) : (
+                    <span className="mt-1 truncate w-full text-center">{file.name}</span>
+                )}
             </div>
         </div>
     );
@@ -46,5 +31,16 @@ export function DirectoryContent({ file }: { file: I_File }) {
     const { getDirFiles } = useFiles();
 
     const files = getDirFiles(file);
-    return <div>{files && <DirectoryFiles files={files} />}</div>;
+    return <div>{files && 
+                <div className="h-full w-full p-4">
+                    <div className="grid grid-cols-auto-fill gap-2">
+                        {files.map((file) => (
+                            <FileInDirectory
+                                key={file.id}
+                                file={file}
+                            />
+                        ))}
+                    </div>
+                </div>
+        }</div>;
 }
